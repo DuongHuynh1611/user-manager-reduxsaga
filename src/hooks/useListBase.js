8c4202ec2f39059bf1dddefe3aebad43c97f8cb9
DuongHuynh1611 <huynhhoaiduong01042002@gmail.com>
2024-09-07 09:47:57 +0700

    update

--- src/hooks/useListBase.js
+++ src/hooks/useListBase.js
@@ -1,57 +1,6 @@
-// import { useState, useEffect } from 'react';
-// import axios from 'axios';
-
-// const useListPage = (apiConfig, params = {}) => {
-//   const [data, setData] = useState([]);
-//   const [pagination, setPagination] = useState({ current: 1, pageSize: 10, total: 0 });
-//   const [loading, setLoading] = useState(false);
-//   const [error, setError] = useState(null);
-
-//   const fetchData = async (page, pageSize) => {
-//     setLoading(true);
-//     try {
-//       const response = await axios.get(apiConfig.getlist, {
-//         params: {
-//           ...params,
-//           page,
-//           pageSize,
-//         },
-//       });   
-//       setData(response.data.items);
-//       setPagination({
-//         current: page,
-//         pageSize,
-//         total: response.data.total,
-//       });
-//     } catch (err) {
-//       setError(err);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchData(pagination.current, pagination.pageSize);
-//   }, [pagination.current, pagination.pageSize]);
-
-//   const handleTableChange = (pagination) => {
-//     fetchData(pagination.current, pagination.pageSize);
-//   };
-
-//   return {
-//     data,
-//     pagination,
-//     loading,
-//     error,
-//     handleTableChange,
-//   };
-// };
-
-// export default useListPage;
-
 import { useState } from 'react';
 
-const useListPage = (initialPageSize = 5, totalItems = 20) => {
+const useListBase = (initialPageSize = 5, totalItems = 20) => {
   const [pagination, setPagination] = useState({ current: 1, pageSize: initialPageSize, total: totalItems });
 
   const handleTableChange = (page, pageSize) => {
@@ -68,4 +17,4 @@ const useListPage = (initialPageSize = 5, totalItems = 20) => {
   };
 };
 
-export default useListPage;
+export default useListBase;
